cfec1ceb764e16702935a5968915903d70625512
Yzbu-eng <64710864+Yzbu-eng@users.noreply.github.com>
2020-05-03 01:23:38 +0300

    Add files via upload

---
+++ classes.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+// Кондитерская "Cделай сам"
+
+class menu {
+
+public:
+
+int f;
+
+char q[30];
+
+int z;
+
+char r[30];
+
+char k[30];
+
+int j;
+
+};
+
+class persone{
+
+public:
+
+char name[20];
+
+char familia[20];
+
+char nomer[12];
+
+};
